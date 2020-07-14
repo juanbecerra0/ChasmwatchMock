d9eaa31e7911522c026949527ba2858b16f56925
Juan Becerra <juanbecerra@u.boisestate.edu>
2020-07-14 23:47:33 +0000

    add code in files types: .md remove code in files types:

--- content.py
+++ content.py
@@ -5,8 +5,8 @@ repo = git.Repo("F:/Chasmwatch")
 mock_repo = git.Repo("F:/GitHub/ChasmwatchMock")
 
 importer = Importer([repo], mock_repo)
-#importer.set_author('becerrajuan007@gmail.com')
+importer.set_author('becerrajuan007@gmail.com')
 #importer.set_author('juanbecerra@u.boisestate.edu')
-importer.set_author('47235983+juanbecerra0@users.noreply.github.com')
+#importer.set_author('47235983+juanbecerra0@users.noreply.github.com')
 
 importer.import_repository()
