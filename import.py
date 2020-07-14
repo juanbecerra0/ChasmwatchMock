2bbfe4690f03d0fb5dd689768af18962128b95d1
juanbecerra0 <47235983+juanbecerra0@users.noreply.github.com>
2020-07-14 23:52:39 +0000

    add code in files types: ,.meta,.controller,.mask,.mat,.physicMaterial,.prefab,.cs,.unity,.shader,.json,.asset,.txt remove code in files types:

--- import.py
+++ import.py
@@ -5,6 +5,6 @@ repo = git.Repo("D:/Chasmwatch")
 mock_repo = git.Repo("D:/ChasmwatchMock")
 
 importer = Importer([repo], mock_repo)
-importer.set_author('juanbecerra@u.boisestate.edu')
+importer.set_author('becerrajuan007@gmail.com')
 
 importer.import_repository()
