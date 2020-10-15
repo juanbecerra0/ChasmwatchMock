979770bad48519dff51aba363bca2897052e0f41
Juan Becerra <juanbecerra@u.boisestate.edu>
2020-10-15 10:51:16 -0600

    Commits 10/15 (1/2)

--- import.py
+++ import.py
@@ -1,11 +1,12 @@
 import git
 from git_contributions_importer import *
 
-repo = git.Repo("D:/Chasmwatch")
-mock_repo = git.Repo("D:/ChasmwatchMock")
+repo = git.Repo("F:/Chasmwatch")
+mock_repo = git.Repo("F:/GitHub/ChasmwatchMock")
 
 importer = Importer([repo], mock_repo)
 #importer.set_author('becerrajuan007@gmail.com')
-importer.set_author('47235983+juanbecerra0@users.noreply.github.com')
+importer.set_author('juanbecerra@u.boisestate.edu')
+#importer.set_author('47235983+juanbecerra0@users.noreply.github.com')
 
 importer.import_repository()
